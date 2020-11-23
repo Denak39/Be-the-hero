4e80df5bc943a6671baa0e69ab4d700a549d86db
Mathias Valverde <mathiasvalverde@hotmail.fr>
2020-11-23 20:43:45 +0100

    new html files + functions

--- index.js
+++ index.js
@@ -1,36 +1,106 @@
-const game = {
+let game = {
   start: {
-    steps: ["goblin", "kitchenLabo", "start"],
+    steps: ["dungeonDoor"],
     description:
       "Here you are, in front of the dungeon’s door. You’ve been hired by King Maverick to kill Lord Ainz Ooal Gown, the Master of this place. No easy task, considering he is the mightiest creature of this world. You think to yourself there must be an easier way to make money...",
   },
+  player: {
+    health: 100,
+  },
+  hallway: {
+    steps: ["goblin", "kitchenLabo", "start"],
+  },
   goblin: {
     health: 10,
-    damage: 1,
   },
   kitchenLabo: {
     steps: ["golem", "bedroom", "start"],
   },
   golem: {
     health: 20,
-    damage: 2,
   },
   bedroom: {
     steps: ["minautor", "bossRoom", "start"],
   },
   minotaur: {
     health: 50,
-    damage: 2,
   },
   end: {
-    steps: ["boss", "start"],
+    steps: ["finalBoss", "start"],
   },
-  boss: {
+  finalBoss: {
     health: 75,
-    damage: 3,
+  },
+  potion: {
+    health: 15,
+    amount: 0,
   },
 };
 
 game["kitchenLabo"].steps[1];
 
 console.log(game);
+
+function rollDicePlayer() {
+  return 1 + Math.floor(Math.random() * 6);
+}
+function rollDiceGoblin() {
+  return 1 + Math.floor(Math.random() * 3);
+}
+function rollDiceGolem() {
+  return 1 + Math.floor(Math.random() * 4);
+}
+function rollDiceMinotaur() {
+  return 1 + Math.floor(Math.random() * 5);
+}
+function rollDiceFinalBoss() {
+  return 1 + Math.floor(Math.random() * 6);
+}
+function dodge() {
+  return 1 + Math.floor(Math.random() * 20);
+}
+function dodgeGoblin() {
+  if (parseFloat(dodge()) > 16) {
+    console.log("You dodged");
+  } else {
+    Swal.fire("You need to attack the goblin");
+  }
+}
+function attackGoblin() {
+  let leaveRoom = document.getElementById("goblinRoom");
+  let button = '<button href="kitchenLab.html">Test</button>';
+  if (game.goblin.health <= 0) {
+    console.log("it's dead");
+  } else {
+    game.goblin.health -= parseFloat(rollDicePlayer());
+    if (game.goblin.health <= 0) {
+      game.potion.amount += 1;
+      leaveRoom.innerHTML = button;
+    } else {
+      game.player.health -= parseFloat(rollDiceGoblin());
+      if (game.player.health <= 0) {
+        Swal.fire('<button href="die.html">Game Over</button>');
+      }
+    }
+  }
+  console.log(game.goblin.health);
+  console.log(game.potion.amount);
+}
+
+function drinkPotion() {
+  if (game.potion.amount > 0) {
+    game.player.health += game.potion.health;
+    game.potion.amount -= 1;
+  }
+  console.log(game.potion.amount);
+  console.log(game.player.health);
+}
+
+function changeRoom() {
+  let boss = ["game.goblin", "game.minotaur", "game.finalBoss", "game.golem"];
+  if (boss.health <= 0) {
+    return changeRoom;
+  } else {
+    return rollDice;
+  }
+}
